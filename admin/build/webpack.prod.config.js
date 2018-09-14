9786941311f133b0a0b6dbcf42c0c2646015de44
xufei0718 <xufei0718@163.com>
2018-09-14 14:02:49 +0800

    整理提交

--- admin/build/webpack.prod.config.js
+++ admin/build/webpack.prod.config.js
@@ -18,7 +18,7 @@ fs.open('./build/env.js', 'w', function(err, fd) {
 
 module.exports = merge(webpackBaseConfig, {
     output: {
-        publicPath: 'https://pac.mybank.cc/ad/dist/',  // 修改 https://iv...admin 这部分为你的服务器域名
+        publicPath: 'http://140.143.10.128:8080/mb-pc/dist/dist/',  // 修改 https://iv...admin 这部分为你的服务器域名
         filename: '[name].[hash].js',
         chunkFilename: '[name].[hash].chunk.js'
     },
